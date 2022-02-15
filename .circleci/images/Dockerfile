bd3b0da482fb8cf59ee0c7466f143fa083b94fa0
Alexander <chernikov.av@edu.spbstu.ru>
2022-02-15 23:26:08 +0300

    fix: return to old test_matrix

--- .circleci/images/Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM cimg/base:stable
-
-RUN apt-get update
-RUN apt-get install libgtest-dev
-RUN apt-get install cmake
--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cmake --build .
-
+g++ tests/test_matrix.cpp -o test_matrix
+chmod +x test_matrix && ./test_matrix
